be2f6538d2a9b082a2961fedc24734b156bac5ce
negoflict255 <negoflict255@gmail.com>
2021-05-30 11:02:23 +0300

    up

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ router.get("/viewnegos/:username", (req, res) => {
             console.log(id);
             connection.query(
                 `SELECT negoid, title FROM negotiation WHERE (mediatoerCode=? OR userCode1=? OR userCode2=?)
-                AND endTime IS NULL`,
+                AND (endTime IS NULL)`,
                 [id, id, id],
                 function (err, resl, fields) {
                     if (err) throw err;
